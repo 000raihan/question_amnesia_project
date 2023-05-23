d074b40dbbf6998c9e4fc0c9fb7b1ed9eb875652
000raihan <000raihanahmad@gmail.com>
2023-05-23 07:33:28 +0600

    updated

--- data/questions.js
+++ data/questions.js
@@ -36,5 +36,102 @@ exports.allQuestion = [
     ans:"Before the onset of amnestic state",
     date:"22/05/2023",
   },
+  {
+    id:4,
+    ques: "Transient global amnesia predominantly affects:",
+    options: {
+      a: "Children",
+      b: "Middle/Late middle aged",
+      c: "Old aged",
+      d: "Newborn",
+    },
+    ans:"Middle/Late middle aged",
+    date:"23/05/2023",
+  },
+  {
+    id:5,
+    ques: "What is the hallmark of TGA episode?",
+    options: {
+      a: "Unable to record new memories",
+      b: "Repetitive questioning",
+      c: "Memory loss",
+      d: "Restlessness",
+    },
+    ans:"Repetitive questioning",
+    date:"24/05/2023",
+  },
+  {
+    id:6,
+    ques: "Amnesia can be achieved/Induced with the following:",
+    options: {
+      a: "Benzodiazepines",
+      b: "Laxatives",
+      c: "Mood stabilizers",
+      d: "Anti-psychotic",
+    },
+    ans:"BBenzodiazepines",
+    date:"25/05/2023",
+  },
+  {
+    id:7,
+    ques: "After any violent crime like homicide, what type of amnesia is seen?",
+    options: {
+      a: "TGA",
+      b: "Retrograde amnesia",
+      c: "Event specific psychogenic amnesia",
+      d: "Neurogenic amnesia",
+    },
+    ans:"TGA",
+    date:"26/05/2023",
+  },
+  {
+    id:8,
+    ques: "8.	Which component of amnesia is usually prominent?",
+    options: {
+      a: "Anterograde component",
+      b: "Retrograde component",
+      c: "Digital component",
+      d: "Both 1 & 2",
+    },
+    ans:"Anterograde component",
+    date:"27/05/2023",
+  },
+  {
+    id:9,
+    ques: "Which events are more vulnerable to Retrograde amnesia?",
+    options: {
+      a: "Older memories",
+      b: "Relatively recent memories",
+      c: "Permanent memories",
+      d: "Sad memories",
+    },
+    ans:"Relatively recent memories",
+    date:"28/05/2023",
+  },
+  {
+    id:10,
+    ques: "Neurogenic state that give rise to amnestic state doesn’t include:",
+    options: {
+      a: "Tumors of sphenoid wing",
+      b: "Tumors of anterior corpus callosum",
+      c: "Tumors of Thalamus",
+      d: "Tumors of medial temporal lobe",
+    },
+    ans:"Tumors of anterior corpus callosum",
+    date:"29/05/2023",
+  },
+  {
+    id:11,
+    ques: "The most common cause of progressive memory impairment in the elderly is:",
+    options: {
+      a: "Alzheimer’s Disease",
+      b: "Head trauma",
+      c: "Herpes simplex encephalitis",
+      d: "Wernicke-korsakoff encephalitis",
+    },
+    ans:"Alzheimer’s Disease",
+    date:"30/05/2023",
+  }
+  
 ];
 
